2cdd61324d751d417295087f557243ac215a2e1d
Nick Rosso <Nicholasrosso@comcast.net>
2017-01-06 12:57:09 -0600

    Features Bug Fix New Game Element
    [Fixed] Problem with images not despawning after they have fallen
    [Fixed] Problem with delete save button not reseting button details
    [Added] Corn Generated per second is calculated when corn collide with
    ground
    [Allowed] Buttons to be on the top of canvas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio - 100, window.innerHeight * window.devicePixelRatio - 100, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var style = { font: 'Nueva Std', fontSize: '60px', fill: '#e7ea73'};
 var buttonStyle = {font: 'Nueva Std', fontSize: '32px', fill: '#159146'};
 
@@ -6,6 +6,7 @@ var buttonStyle = {font: 'Nueva Std', fontSize: '32px', fill: '#159146'};
 function preload(){
 	game.load.image('background', 'assets/cornfieldbackground.png');
 	game.load.image('cornclick', 'assets/clickbutton.png');
+	game.load.image('ground', 'assets/platform.png');
 	game.load.image('upgradecornclick', 'assets/upgradebutton.png');
 	game.load.image('cornkernel', 'assets/kernels.png');
 	game.load.image('deletesave', 'assets/deleteSave.png')
@@ -13,16 +14,25 @@ function preload(){
 
 function create(){
 	loadGame();
-	game.stage.backgroundColor = '#e7ea73';
 	game.stage.disableVisibilityChange = true;
 	game.physics.startSystem(Phaser.Physics.Arcade)
 
 	background = game.add.tileSprite(0,0, window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, 'background');
+	
+	platforms = game.add.group();
+	platforms.enableBody = true;
+
+	fallingCorn = game.add.group();
+	fallingCorn.enableBody = true;
+
+	var cornDrawLimit = platforms.create(0, game.world.height - 64, 'ground');
+	cornDrawLimit.scale.setTo(2,2);
+	cornDrawLimit.body.immovable = true;
 
 	cornClick = game.add.button(game.world.centerX - 170, game.world.centerY - 60, 'cornclick', generateCorn, this, 2,1,0);
 	upgradeClick = game.add.button(game.world.centerX - 320, game.world.centerY + 30, 'upgradecornclick', upgradeClickLevel, this, 2, 1, 0);
 	upgradeCornRate = game.add.button(game.world.centerX , game.world.centerY + 30, 'upgradecornclick', upgradeCornGainRate, this, 2, 1, 0);
-	deleteSaveButton = game.add.button(0, game.world.height - 100, 'deletesave', deleteGameData, this, 2, 1, 0);
+	deleteSaveButton = game.add.button(0, game.world.height - 60, 'deletesave', deleteGameData , this, 2, 1, 0);
 	totalCornText = game.add.text(16, 16, 'Corn: '+ gameData.totalCorn, style);
 
 	clickLevelInfoText = game.add.text(game.world.centerX - 250, game.world.centerY + 40, '+ Corn Per Click', buttonStyle);
@@ -33,15 +43,15 @@ function create(){
 	cornRateLevelText = game.add.text(game.world.centerX + 10, game.world.centerY + 65, 'Corn Per Second: ' + gameData.cornGainRateLevel, buttonStyle);
 	cornRateLevelCostText = game.add.text(game.world.centerX + 10, game.world.centerY + 90,'Cost: '+ gameData.upgradeCornRateCost, buttonStyle);
 
-	game.time.events.loop(Phaser.Timer.SECOND, cornGainRate, this);
-	game.time.events.loop(Phaser.Timer.SECOND * 2, renderCorn, this);
-	game.time.events.loop(Phaser.Timer.SECOND, saveGame,this);
+	game.time.events.loop(Phaser.Timer.SECOND, renderCorn, this);
+	game.time.events.loop(Phaser.Timer.SECOND, saveGame, this);
+	game.time.events.loop(Phaser.Timer.SECOND / 5, updateButtonDetails, this)
 }
 
 function update(){
-
-
-
+	
+	game.physics.arcade.overlap(fallingCorn, platforms, killCorn, null, this);
+	
 }
 
 
@@ -55,9 +65,6 @@ function upgradeClickLevel(){
 		gameData.cornClickLevel++;
 		gameData.totalCorn -= gameData.upgradeClickCost;
 		gameData.upgradeClickCost = Math.floor(10 * Math.pow(1.5, gameData.cornClickLevel));
-		clickLevelCostText.text = 'Cost: ' + gameData.upgradeClickCost;
-		clickLevelText.text = 'Corn Per Click: ' + gameData.cornClickLevel;
-		totalCornText.text = 'Corn: ' + gameData.totalCorn;
 	}
 }
 
@@ -66,21 +73,11 @@ function upgradeCornGainRate(){
 		gameData.cornGainRateLevel++;
 		gameData.totalCorn -= gameData.upgradeCornRateCost;
 		gameData.upgradeCornRateCost = Math.floor(25 * Math.pow(1.5, gameData.cornGainRateLevel));
-		cornRateLevelCostText.text = 'Cost: ' + gameData.upgradeCornRateCost;
-		cornRateLevelText.text = 'Corn Per Second: ' + gameData.cornGainRateLevel;
-		totalCornText.text = 'Corn: ' + gameData.totalCorn;
 	}
 }
 
-function cornGainRate(){
-	gameData.totalCorn += gameData.cornGainRateLevel;
-	totalCornText.text = 'Corn: ' + gameData.totalCorn;
-
-}
 
 function renderCorn(){
-	fallingCorn = game.add.group();
-	fallingCorn.enableBody = true;
 
 	for (var i = 0; i < gameData.cornGainRateLevel; i++)
 	{
@@ -114,4 +111,18 @@ function deleteGameData(){
 	}
 	localStorage.setItem("savedData", JSON.stringify(gameData));
 	console.log("Save Formatted");
+}
+
+function updateButtonDetails(){
+	clickLevelCostText.text = 'Cost: ' + gameData.upgradeClickCost;
+	clickLevelText.text = 'Corn Per Click: ' + gameData.cornClickLevel;
+	cornRateLevelCostText.text = 'Cost: ' + gameData.upgradeCornRateCost;
+	cornRateLevelText.text = 'Corn Per Second: ' + gameData.cornGainRateLevel;
+	totalCornText.text = 'Corn: ' + gameData.totalCorn;
+
+}
+function killCorn(cornImage){
+	console.log("hit");
+	cornImage.kill();
+	gameData.totalCorn += 1;
 }
